0eb83d9b4bd9d254a709c35462f1872394cdd11e
alexey.tarasenkov <uran-2010@mail.ru>
2020-10-21 16:58:11 +0300

    Ready

--- src/main/java/com/accenture/JavaQuestions/Controllers/HelloController.java
+++ src/main/java/com/accenture/JavaQuestions/Controllers/HelloController.java
@@ -3,12 +3,12 @@ package com.accenture.JavaQuestions.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello(Model model){
-        model.addAttribute("title", "Hello World");
+    public String hello(){
         return "hello";
     }
 }
